3465e89e3735a69b8e989557c28617b2c654f8fa
shaogan10555015 <shaogan10555015@gmail.com>
2023-05-30 18:06:20 +0800

    header按鈕統一封裝、myClass BUG修正

---
+++ SquirrelOJ/public/control.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    console.log("HI");
+    const gotoLoginHTML = '<button class="nav-button goto-login">登入</button>';
+    const gotoProblemsHTML = '<button class="nav-button goto-problems">題庫</button>';
+    $(".buttons-container").append(gotoLoginHTML);
+    $(".buttons-container").append(gotoProblemsHTML);
+    $(".goto-login").on("click", function () {
+        window.location.href = "/login.html";
+        console.log("login被點擊了！");
+    });
+
+    $(".goto-problems").on("click", function () {
+        window.location.href = '/problems.html';
+        console.log("problems被點擊了！");
+    });
+});
