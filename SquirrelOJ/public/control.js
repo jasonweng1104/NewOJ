a1ebe375413401a8e5be232739f70e5f45256ed5
shaogan10555015 <shaogan10555015@gmail.com>
2023-06-02 16:47:29 +0800

    登出鈕拿去

--- SquirrelOJ/public/control.js
+++ SquirrelOJ/public/control.js
@@ -1,13 +1,18 @@
 $(document).ready(function () {
     const gotoLoginHTML = '<button class="nav-button goto-login">登入</button>';
     const gotoProblemsHTML = '<button class="nav-button goto-problems">題庫</button>';
+    const gotoLogoutHTML = '<button class="nav-button goto-logout">登出</button>';
     $(".buttons-container").append(gotoLoginHTML);
     $(".buttons-container").append(gotoProblemsHTML);
+    $(".buttons-container").append(gotoLogoutHTML);
     $(".goto-login").on("click", function () {
         window.location.href = "/login.html";
         console.log("login被點擊了！");
     });
-
+    $(".goto-logout").on("click", function () {
+        //登出鈕在這裡
+        console.log("logout被點擊了！");
+    });
     $(".goto-problems").on("click", function () {
         window.location.href = '/problems.html';
         console.log("problems被點擊了！");
