f5ee4232079891210d879c7f8d15c7a05cfd7672
a12355765 <a12355765@gmail.com>
2023-06-09 02:29:16 +0800

    判分功能

--- SquirrelOJ/q1.test/judge.py
+++ SquirrelOJ/q1.test/judge.py
@@ -1,4 +1,5 @@
 import subprocess#匯入subprocess模組(執行其他py檔)
+import time
 with open('code.txt', 'r') as code:
     ex_code = code.read()#讀入使用者輸入的code.txt
     
@@ -8,22 +9,34 @@ with open('code_user.py', 'w') as code:
 with open('input.txt', 'r') as input:
     ex_input = input.read().strip()#讀入測資
 
-result=subprocess.run(['python',"code_user.py"],input=ex_input,capture_output=True,text=True)#執行code_user.py檔,並將測資丟進去跑
+start=time.perf_counter()
+
+try:
+    result=subprocess.run(['python',"code_user.py"],input=ex_input,timeout=3,capture_output=True,text=True,check=True)#執行code_user.py檔,並將測資丟進去跑
+except subprocess.CalledProcessError as e:
+    print(e.stderr)
+    exit()
+except (subprocess.TimeoutExpired,RuntimeError):#如果超時的話
+    print("TLE")
+    exit()
+
+end=time.perf_counter()
 
 output = result.stdout.strip()#code_user.py的輸出結果
 
 with open('answer.txt', 'r') as ans:#讀入正確答案
     ex_ans = ans.read()
 
-print(output)       #-
+print(output)   #-
 print("------")     #-
 print(ex_ans)       #-
-print("------")     #-輸出「code_user.py的輸出結果」與「正確答案」
-
+print("------")     #-輸出「code_user.py的輸出結果」與「正確答案」(若超時則輸出TLE)
 if output==ex_ans:#判斷兩者是否相同，相同輸出True
-    print("True")
+    print("True(%f)"%(end-start))
 else:
-    print("False")
+    print("False(%f)"%(end-start))
+
+
 
 
 
